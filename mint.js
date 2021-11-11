d295d46969c4c299dfdb29ce301965daf2250a6b
willsabia <61212211+willsabia@users.noreply.github.com>
2021-11-11 16:39:02 -0500

    trial mint

--- mint.js
+++ mint.js
@@ -1,25 +1,348 @@
-'use strict';
+"use strict";
 
 const e = React.createElement;
+const useState = React.useState;
+const useEffect = React.useEffect;
 
-class mint extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+const ConnectAndMint = ({ reserve }) => {
+  const [signedIn, setSignedIn] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [scripturesContract, setscripturesContract] = useState(null);
+  const [totalSupply, setTotalSupply] = useState(null);
+  const [saleStarted, setSaleStarted] = useState(false);
+  const [whitelistedFor1, setWhitelistedFor1] = useState(false);
+  const [whitelistedFor2, setWhitelistedFor2] = useState(false);
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+  useEffect(() => {
+    (async () => {
+      if (typeof window.web3 !== "undefined") {
+        // Use existing gateway
+        window.web3 = new Web3(window.ethereum);
+      } else {
+        alert("No ETH interface plugged. Using read-only.");
+      }
+      const scripturesContract = new window.web3.eth.Contract(ABI, ADDRESS);
+      setscripturesContract(scripturesContract);
+
+      const totalSupply = await scripturesContract.methods.totalSupply().call();
+      setTotalSupply(totalSupply);
+
+      const saleIsActive = await scripturesContract.methods.saleIsActive().call();
+      setSaleStarted(saleIsActive);
+    })();
+  }, []);
+
+  async function signIn() {
+    if (typeof window.web3 !== "undefined") {
+      // Use existing gateway
+      window.web3 = new Web3(window.ethereum);
+    } else {
+      alert("No ETH interface plugged. Using read-only.");
     }
 
+    window.ethereum
+      .enable()
+      .then((accounts) => {
+        window.web3.eth.net
+          .getNetworkType()
+          // check if connected network is mainnet
+          .then((network) => {
+            if (network != "main") {
+              alert("You are on " + network + " network. Change network to Ethereum mainnet.");
+            }
+          });
+        let walletAddress = accounts[0];
+        setWalletAddress(walletAddress);
+        setSignedIn(true);
+        callContractData(walletAddress);
+      })
+      .catch((err) => {
+        // Handle error. Likely the user rejected the login
+        console.error(err);
+      });
+  }
+
+  async function signOut() {
+    setSignedIn(false);
+  }
+
+  async function callContractData(walletAddress) {
+    const scripturesContract = new window.web3.eth.Contract(ABI, ADDRESS);
+    setscripturesContract(scripturesContract);
+
+    const totalSupply = await scripturesContract.methods.totalSupply().call();
+    setTotalSupply(totalSupply);
+
+    const saleIsActive = await scripturesContract.methods.saleIsActive().call();
+    setSaleStarted(saleIsActive);
+
+    const whitelistOneMint = await scripturesContract.methods.whitelistOneMint(walletAddress).call();
+    setWhitelistedFor1(whitelistOneMint);
+
+    const whitelistTwoMint = await scripturesContract.methods.whitelistTwoMint(walletAddress).call();
+    setWhitelistedFor2(whitelistTwoMint);
+  }
+
+  function getScripturesPriceEach(n) {
+    if (n === 30) {
+      return BigNumber("60000000000000000"); // 0.06 ETH
+    } else if (n === 15) {
+      return BigNumber("70000000000000000"); // 0.07 ETH
+    } else if (n === 5) {
+      return BigNumber("75000000000000000"); // 0.075 ETH
+    } else {
+      return BigNumber("80000000000000000"); // 0.08 ETH
+    }
+  }
+
+  async function mintScripturess(n) {
+    if (!scripturesContract) {
+      return;
+    }
+
+    try {
+      const price = getScripturesPriceEach(n).multipliedBy(n).toString();
+
+      // const gasAmount = await scripturesContract.methods.mintScripturess(n).estimateGas({ from: walletAddress, value: price });
+
+      // Using reserveMintScripturess() instead of mintScripturess(n) to estimate gas...
+      // This is a gross hack and it frightens me, but I think it works. // alima
+      const gasAmount = await scripturesContract.methods
+        .reserveMintScripturess()
+        .estimateGas({ from: walletAddress, value: "0" });
+
+      console.log("estimated gas", gasAmount);
+      console.log({ from: walletAddress, value: price });
+
+      await scripturesContract.methods
+        .mintScripturess(n)
+        .send({ from: walletAddress, value: price, gas: String(gasAmount) })
+        .on("transactionHash", (hash) => {
+          console.log("transactionHash", hash);
+        });
+    } catch (err) {
+      alert(JSON.stringify(err));
+    }
+  }
+
+  async function reserveMintScripturess() {
+    if (!scripturesContract) {
+      return;
+    }
+
+    try {
+      const price = "0";
+
+      const gasAmount = await scripturesContract.methods
+        .reserveMintScripturess()
+        .estimateGas({ from: walletAddress, value: price });
+
+      console.log("estimated gas", gasAmount);
+      console.log({ from: walletAddress, value: price });
+
+      await scripturesContract.methods
+        .reserveMintScripturess()
+        .send({ from: walletAddress, value: price, gas: String(gasAmount) })
+        .on("transactionHash", (hash) => {
+          console.log("transactionHash", hash);
+        });
+    } catch (err) {
+      alert(JSON.stringify(err));
+    }
+  }
+
+  function format(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  const Hr = () => e("hr");
+  const Br = () => e("br");
+
+  const SignInButton = () => {
+    return e("button", { className: "connect-button", onClick: signIn }, `Connect to MetaMask`);
+  };
+  const SignOutButton = () => {
+    return e("button", { className: "connect-button", onClick: signOut }, `Disconnect from MetaMask`);
+  };
+
+  const MiniHeader = () => {
+    return e("div", null, AmountMinted(), Hr(), e("h3", { className: "mint-your-scriptures" }, "Mint your Scriptures:"));
+  };
+
+  const AmountMinted = () => {
+    if (totalSupply == null) return null;
     return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      "div",
+      { className: "amount-minted" },
+      e("div", { className: "current-supply" }, "Current Supply:"),
+      e("div", null, `${format(totalSupply)} / ${format(10000)}`)
+    );
+  };
+
+  const MintButton = (n) => {
+    const priceEach = getScripturesPriceEach(n).dividedBy("1e18");
+    const priceAll = priceEach.multipliedBy(n);
+    const text = `Mint ${n} Scriptures${n > 1 ? "s" : ""} - ${priceAll} ETH (${priceEach} each)`;
+
+    if (!signedIn) {
+      return e("div", { className: "mint-button" }, text);
+    } else {
+      return e("div", { className: "mint-button" }, e("a", { onClick: () => mintScripturess(n) }, text));
+    }
+  };
+
+  const WhitelistedNotice = (n) => {
+    return e(
+      "div",
+      { className: "whitelisted-notice" },
+      `Congrats! Your wallet is whitelisted to reserve ${n} free Scriptures${
+        n > 1 ? "s" : ""
+      } from the Community Reserve! `,
+      e("a", { href: "reserve.html" }, "Click to view!")
+    );
+  };
+
+  const WhitelistedNoticeReserve = (n) => {
+    if (n === 0) {
+      return e(
+        "div",
+        { className: "whitelisted-notice-reserve" },
+        `Sorry, but your address (${walletAddress}) is not whitelisted.`
+      );
+    }
+    return e(
+      "div",
+      { className: "whitelisted-notice-reserve" },
+      `Congrats! Your wallet is whitelisted to reserve ${n} free Scriptures${n > 1 ? "s" : ""} from the ` +
+        `Community Reserve! Click below to claim if you haven't already (gas not included):`
+    );
+  };
+
+  const MintReserveButton = () => {
+    return e(
+      "div",
+      { className: "mint-reserve-button" },
+      e("a", { onClick: () => reserveMintScripturess() }, `Mint Scripturess`)
+    );
+  };
+
+  const WalletNotice = () => {
+    return e("div", { className: "wallet-show" }, "Connected wallet: " + walletAddress);
+  };
+
+  const NoWalletNotice = () => {
+    return e("div", { className: "wallet-show" }, "No wallet connected");
+  };
+
+  /*
+   if (!saleStarted) {
+    return e("div", { className: "sale-notice" }, "The sale has not started yet.");
+   }
+  */
+
+  if (!signedIn) {
+    return e(
+      "div",
+      { className: "centered-text" },
+      MiniHeader(),
+      Br(),
+      SignInButton(),
+      Br(),
+      Br(),
+      NoWalletNotice(),
+      Br(),
+      MintButton(1),
+      MintButton(5),
+      MintButton(15),
+      MintButton(30)
     );
   }
+
+  if (reserve) {
+    if (whitelistedFor1) {
+      return e(
+        "div",
+        { className: "centered-text" },
+        MiniHeader(),
+        Br(),
+        SignOutButton(),
+        Br(),
+        Br(),
+        WalletNotice(),
+        Br(),
+        WhitelistedNoticeReserve(1),
+        MintReserveButton()
+      );
+    }
+    if (whitelistedFor2) {
+      return e(
+        "div",
+        { className: "centered-text" },
+        MiniHeader(),
+        Br(),
+        SignOutButton(),
+        Br(),
+        Br(),
+        WalletNotice(),
+        Br(),
+        WhitelistedNoticeReserve(2),
+        MintReserveButton()
+      );
+    }
+    return e(
+      "div",
+      { className: "centered-text" },
+      MiniHeader(),
+      Br(),
+      SignOutButton(),
+      Br(),
+      WhitelistedNoticeReserve(0)
+    );
+  }
+
+  return e(
+    "div",
+    null,
+    MiniHeader(),
+    Br(),
+    SignOutButton(),
+    Br(),
+    Br(),
+    WalletNotice(),
+    Br(),
+    MintButton(1),
+    MintButton(5),
+    MintButton(15),
+    MintButton(30),
+    e(
+      "div",
+      { className: "whitelisted-notices" },
+      whitelistedFor1 ? WhitelistedNotice(1) : null,
+      whitelistedFor2 ? WhitelistedNotice(2) : null
+    )
+  );
+};
+
+const mint = document.querySelector("#mint");
+
+if (mint) {
+  ReactDOM.render(
+    e(() => ConnectAndMint({ reserve: false })),
+    mint
+  );
 }
 
-const domContainer = document.querySelector('#mint');
-ReactDOM.render(e(mint), domContainer);
+const mintReserve = document.querySelector("#mint-reserve");
+if (mintReserve) {
+  ReactDOM.render(
+    e(() => ConnectAndMint({ reserve: true })),
+    mintReserve
+  );
+}
+return (
+<button onClick={()=> mintScripturess()} >
+  mint
+</button>
+);
+
+
