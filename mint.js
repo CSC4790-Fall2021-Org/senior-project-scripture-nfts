2b5caa0fed0ec9a557d7a70c483197d81986e254
“ipaladinoVillanova” <ipaladin@villanova.edu>
2021-11-11 15:49:26 -0500

    starter react code

---
+++ mint.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class mint extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#mint');
+ReactDOM.render(e(mint), domContainer);
