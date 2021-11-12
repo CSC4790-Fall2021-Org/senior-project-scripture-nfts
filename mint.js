a2baceaf1137becb13845a00fdf9772e36ff00b6
“ipaladinoVillanova” <ipaladin@villanova.edu>
2021-11-11 21:27:00 -0500

    Update mint.js

--- mint.js
+++ mint.js
@@ -1,24 +1,14 @@
 "use strict";
 
-import Head from 'next/head'
-import Web3 from "web3";
-import { useState, useEffect } from 'react';
-
-import {ADDRESS, ABI} from "../config.js"
-
 export default function Mint() {
 
-  // FOR WALLET
   const [signedIn, setSignedIn] = useState(false)
 
   const [walletAddress, setWalletAddress] = useState(null)
 
-  // FOR MINTING
   const [how_many_scriptures, set_how_many_scriptures] = useState(1)
 
   const [scriptureContract, setScriptureContract] = useState(null)
-
-  // INFO FROM SMART Contract
 
   const [totalSupply, setTotalSupply] = useState(0)
 
@@ -34,7 +24,6 @@ export default function Mint() {
 
   async function signIn() {
     if (typeof window.web3 !== 'undefined') {
-      // Use existing gateway
       window.web3 = new Web3(window.ethereum);
      
     } else {
@@ -44,7 +33,6 @@ export default function Mint() {
     window.ethereum.enable()
       .then(function (accounts) {
         window.web3.eth.net.getNetworkType()
-        // checks if connected network is mainnet (change this to rinkeby if you wanna test on testnet)
         .then((network) => {console.log(network);if(network != "main"){alert("You are on " + network+ " network. Change network to mainnet or you won't be able to do anything here")} });  
         let wallet = accounts[0]
         setWalletAddress(wallet)
@@ -53,12 +41,9 @@ export default function Mint() {
 
   })
   .catch(function (error) {
-  // Handle error. Likely the user rejected the login
   console.error(error)
   })
   }
-
-//
 
   async function signOut() {
     setSignedIn(false)
@@ -105,69 +90,27 @@ export default function Mint() {
     
   };
 
-  
-
-
 
   return (
     
-    <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
-    <Head>
-      <title> Scriptures Company</title>
-      <link rel="icon" href="/images/favicon.jpg" />
-
-      <meta property="og:title" content=" Scriptures Co." key="ogtitle" />
-      <meta property="og:description" content="Here at  Scriptures company, we specialise in the world's finest digital scriptures. We've put together a team spanning 3 continents, to bring you some of the most NUTRITIOUS and DELICIOUS
-scriptures out known to man." key="ogdesc" />
-      <meta property="og:type" content="website" key="ogtype" />
-      <meta property="og:url" content="https://scriptures.co/" key="ogurl"/>
-      <meta property="og:image" content="https://scriptures.co/images/Hola.gif" key="ogimage"/>
-      <meta property="og:site_name" content="https://scriptures.co/" key="ogsitename" />
-
-      <meta name="twitter:card" content="summary_large_image" key="twcard"/>
-      <meta property="twitter:domain" content="scriptures.co" key="twdomain" />
-      <meta property="twitter:url" content="https://scriptures.co/" key="twurl" />
-      <meta name="twitter:title" content=" Scriptures Co." key="twtitle" />
-      <meta name="twitter:description" content="Here at  Scriptures company, we specialise in the world's finest digital scriptures. We've put together a team spanning 3 continents, to bring you some of the most NUTRITIOUS and DELICIOUS
-scriptures out known to man." key="twdesc" />
-      <meta name="twitter:image" content="https://scriptures.co/images/Hola.gif" key="twimage" />
-    </Head>
+    <div>
+    
 
 
     <div >
-        <div className="flex items-center justify-between w-full border-b-2	pb-6">
-          <a href="/" className=""><img src="images/Hola.gif" width="108" alt="" className="logo-image" /></a>
-          <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-            <a href="/#about" className="text-4xl text-white hover:text-black m-6">About</a>
-            <a href="/mint" className="text-4xl text-white hover:text-black m-6">Mint!</a>
-            <a href="/#traits" className="text-4xl text-white hover:text-black m-6">Scripture traits</a>
-            <a href="/#roadmap" className="text-4xl text-white hover:text-black m-6">Roadmap</a>
-            <a href="/#team" className="text-4xl text-white hover:text-black m-6">Team</a>
-            <a href="/#contact" className="text-4xl text-white hover:text-black m-6">Contact</a>
-            <a href="https://twitter.com/scripturesco" className="text-4xl  hover:text-white m-6 text-blau">TWITTER</a>
-            <a href="https://discord.gg/8Wk9Hp6UyV" className="text-4xl  hover:text-white m-6 text-blau">DISCORD</a>
-          </nav>
-           
-        </div>
-        <div className="flex auth my-8 font-bold  justify-center items-center vw2">
-          {!signedIn ? <button onClick={signIn} className="montserrat inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Connect Wallet with Metamask</button>
+        
+        <div>
+          {!signedIn ? <button onClick={signIn}>Connect Wallet with Metamask</button>
           :
-          <button onClick={signOut} className="montserrat inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Wallet Connected: {walletAddress}</button>}
+          <button onClick={signOut}> {walletAddress}</button>}
         </div>
       </div>
+      <div>    
+        <div>
+          <div>
+              <span>TOTAL SCRIPTURES MINTED:  <span > {!signedIn ?  <>-</>  :  <>{totalSupply}</> } / 8888</span></span>
 
-      <div className="md:w-2/3 w-4/5">
-     
-      
-        <div className="mt-6 border-b-2 py-6">
-
-          <div className="flex flex-col items-center">
-
-              <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none my-4 ">TOTAL SCRIPTURES MINTED:  <span className="text-blau text-6xl"> {!signedIn ?  <>-</>  :  <>{totalSupply}</> } / 8888</span></span>
-
-              <div id="mint" className="flex justify-around  mt-8 mx-6">
-                <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">GIMME</span>
-                
+              <div id="mint">     
                 <input 
                                     type="number" 
                                     min="1"
@@ -175,18 +118,12 @@ scriptures out known to man." key="twdesc" />
                                     value={how_many_scriptures}
                                     onChange={ e => set_how_many_scriptures(e.target.value) }
                                     name="" 
-                                    className="Poppitandfinchsans pl-4 text-4xl  inline bg-grey-lighter  py-2 font-normal rounded text-grey-darkest  font-bold"
                                 />
-                
-                <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">SCRIPTURES!</span>
-  
               </div>
               {saleStarted ? 
-              <button onClick={() => mintScripture(how_many_scriptures)} className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT {how_many_scriptures} scriptures for {(scripturePrice * how_many_scriptures) / (10 ** 18)} ETH + GAS</button>        
-                : <button className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
-          
-            }
-              
+              <button onClick={() => mintScripture(how_many_scriptures)}>MINT {how_many_scriptures} scriptures for {(scripturePrice * how_many_scriptures) / (10 ** 18)} ETH + GAS</button>        
+                : <button>SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
+            }    
           </div> 
           </div>
 
@@ -195,5 +132,5 @@ scriptures out known to man." key="twdesc" />
   )
   
 }
-const domContainer = document.querySelector('#mint'); ReactDOM.render(e(mint), domContainer);
-
+const domContainer = document.querySelector('#mint');
+ReactDOM.render(e(mint), domContainer);
